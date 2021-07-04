0edecf36ad6e48e99353e85d59422feca944ad2f
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-04 20:06:58 +0530

    initial commit

---
+++ src/main/java/com/pkglobal/CustomerPublihserApplication.java
@@ -0,0 +1,13 @@
+package com.pkglobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPublihserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPublihserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkglobal/CustomerPublihser/CustomerPublihserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkglobal.CustomerPublihser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPublihserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
