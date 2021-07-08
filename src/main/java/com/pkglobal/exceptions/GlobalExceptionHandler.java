4882d0f6a7aa0512a54f8e82e2c5fd42c16e9dac
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-08 16:49:28 +0530

    commit

--- src/main/java/com/pkglobal/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/pkglobal/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.pkglobal.exceptions;
 
 import java.util.stream.Collectors;
+
 import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.oauth2.common.exceptions.UnauthorizedClientException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -15,58 +19,64 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 import com.pkglobal.constant.PublisherConstants;
 import com.pkglobal.model.ErrorResponse;
 
-
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public final ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpServletRequest request) {
+		String errors = ex.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage())
+				.collect(Collectors.joining(","));
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setStatus(PublisherConstants.ERROR.getValue());
+		errorResponse.setMessage(errors);
+		errorResponse.setErrorType(ex.getClass().getName());
+		logger.error("ErrorResponse :{}", errorResponse);
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public final ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex, HttpServletRequest request) {
-        String errors = ex.getBindingResult().getFieldErrors().stream()
-                .map(x -> x.getDefaultMessage()).collect(Collectors.joining(","));
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setStatus(PublisherConstants.ERROR.getValue());
-        errorResponse.setMessage(errors);
-        errorResponse.setErrorType(ex.getClass().getName());
-        logger.error("ErrorResponse :{}", errorResponse);
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<ErrorResponse> noHandlerFound(NoHandlerFoundException ex, HttpServletRequest request) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setStatus(PublisherConstants.ERROR.getValue());
+		errorResponse.setMessage(ex.getMessage());
+		errorResponse.setErrorType(ex.getClass().getName());
+		logger.error("ErrorResponse :{}", errorResponse);
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+	}
 
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<ErrorResponse> noHandlerFound(NoHandlerFoundException ex,
-            HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setStatus(PublisherConstants.ERROR.getValue());
-        errorResponse.setMessage(ex.getMessage());
-        errorResponse.setErrorType(ex.getClass().getName());
-        logger.error("ErrorResponse :{}", errorResponse);
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
+	@ExceptionHandler(UnauthorizedClientException.class)
+	public ResponseEntity<ErrorResponse> unauthorizedHandler(UnauthorizedClientException ex,
+			HttpServletRequest request) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setStatus(PublisherConstants.ERROR.getValue());
+		errorResponse.setMessage(ex.getMessage());
+		errorResponse.setErrorType(ex.getClass().getName());
+		logger.error("ErrorResponse :{}", errorResponse);
+		return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+	}
 
-    @ExceptionHandler(UnauthorizedClientException.class)
-    public ResponseEntity<ErrorResponse> unauthorizedHandler(UnauthorizedClientException ex,
-            HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setStatus(PublisherConstants.ERROR.getValue());
-        errorResponse.setMessage(ex.getMessage());
-        errorResponse.setErrorType(ex.getClass().getName());
-        logger.error("ErrorResponse :{}", errorResponse);
-        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-    }
+	@ExceptionHandler(CustomerPublishServiceException.class)
+	public final ResponseEntity<ErrorResponse> handleException(CustomerPublishServiceException ex,
+			HttpServletRequest request) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setStatus(PublisherConstants.ERROR.getValue());
+		errorResponse.setMessage(ex.getMessage());
+		errorResponse.setErrorType(ex.getClass().getName());
+		logger.error("ErrorResponse :{}", errorResponse);
+		return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
-    @ExceptionHandler(CustomerPublishServiceException.class)
-    public final ResponseEntity<ErrorResponse> handleException(CustomerPublishServiceException ex,
-            HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setStatus(PublisherConstants.ERROR.getValue());
-        errorResponse.setMessage(ex.getMessage());
-        errorResponse.setErrorType(ex.getClass().getName());
-        logger.error("ErrorResponse :{}", errorResponse);
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	@ExceptionHandler({ HttpMessageNotReadableException.class, MissingRequestHeaderException.class })
+	public final ResponseEntity<ErrorResponse> requestValidationException(Exception ex, HttpServletRequest request) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setStatus(PublisherConstants.ERROR.getValue());
+		errorResponse.setMessage(ex.getLocalizedMessage());
+		errorResponse.setErrorType(ex.getClass().getSimpleName());
+		logger.error("ErrorResponse : {}", errorResponse);
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
 
 }
