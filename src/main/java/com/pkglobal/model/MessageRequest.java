a63bec447431801506fc9e378aef038827c39ad1
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-08 13:20:30 +0530

    changed

--- src/main/java/com/pkglobal/model/MessageRequest.java
+++ src/main/java/com/pkglobal/model/MessageRequest.java
@@ -40,7 +40,7 @@ public class MessageRequest {
 	@JsonProperty("birthDate")
 	@JsonFormat(pattern = "dd-MM-yyyy")
 	@NotEmpty(message = "birthDate should not be empty.")
-	@Pattern(regexp = "^((0[1-9]|[12][0-9]|3[01]-(0[1-9]|1[012])-(19|2[0-9])[0-9]{2}))$", message = "The field birthdate should be in dd-mm-yyyy format")
+	@Pattern(regexp = "^((0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[012])-(19|2[0-9])[0-9]{2}))$", message = "The field birthdate should be in dd-mm-yyyy format")
 	private String birthDate = null;
 
 	@JsonProperty("country")
