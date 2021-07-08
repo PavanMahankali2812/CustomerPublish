3474f8a57dd26d972f6db975466d40093a38b3a9
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-08 11:20:52 +0530

    changes

--- src/test/java/co/pkglobal/service/DefaultCustomerPublishServiceTest.java
+++ src/test/java/co/pkglobal/service/DefaultCustomerPublishServiceTest.java
@@ -2,6 +2,7 @@ package co.pkglobal.service;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,69 +18,65 @@ import com.pkglobal.model.Address;
 import com.pkglobal.model.CustomerStatusEnum;
 import com.pkglobal.model.MessageRequest;
 import com.pkglobal.model.MessageResponse;
-import com.pkglobal.service.CustomerPublishService;
 import com.pkglobal.service.DefaultCustomerPublishService;
-
 
 @ExtendWith(MockitoExtension.class)
 class DefaultCustomerPublishServiceTest {
 
-    @InjectMocks
-    private DefaultCustomerPublishService defaultPublishService;
+	@InjectMocks
+	private DefaultCustomerPublishService defaultPublishService;
 
-    @Mock
-    private KafkaTemplate<String, Object> kafkaTemplate;
+	@Mock
+	private KafkaTemplate<String, Object> kafkaTemplate;
 
-    @Mock
-    private DefaultMessageRequestConverter messageRequestConverter;
+	@Mock
+	private DefaultMessageRequestConverter messageRequestConverter;
 
-    @Mock
-    private DefaultMessageRequestMaskConverter messageRequestMaskConverter;
+	@Mock
+	private DefaultMessageRequestMaskConverter messageRequestMaskConverter;
 
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
+	@Test
+	void testPublishMessageWithSuccess() {
+		MessageRequest messageRequest = buildMessageRequestObject();
+		MessageResponse result = defaultPublishService.publishMessage(messageRequest);
+		assertNotNull(result);
+		assertEquals(buildMessageResponse(), result);
+	}
 
-    @Test
-    void testPublishMessageWithSuccess()  {
-        MessageRequest messageRequest = buildMessageRequestObject();
-        MessageResponse result = defaultPublishService.publishMessage(messageRequest);
-        assertNotNull(result);
-        assertEquals(buildMessageResponse(), result);
-    }
+	private MessageRequest buildMessageRequestObject() {
+		MessageRequest messageRequest = new MessageRequest();
+		Address address = buildAddressObject();
+		messageRequest.setAddress(address);
+		messageRequest.setCountry("India");
+		messageRequest.setCountryCode("IN");
+		messageRequest.setCustomerNumber("CUST123456");
+		messageRequest.setCustomerStatus(CustomerStatusEnum.OPEN);
+		messageRequest.setEmail("Pavan@gmail.com");
+		messageRequest.setFirstName("pavan pavan");
+		messageRequest.setLastName("kumar kumar");
+		messageRequest.setBirthDate("12-02-2020");
+		messageRequest.setMobileNumber("9603335300");
+		return messageRequest;
+	}
 
+	private Address buildAddressObject() {
+		Address address = new Address();
+		address.setAddressLine1("lakkavaram");
+		address.setAddressLine2("west");
+		address.setPostalCode("534451");
+		address.setStreet("jungle");
+		return address;
+	}
 
-    private MessageRequest buildMessageRequestObject() {
-        MessageRequest messageRequest = new MessageRequest();
-        Address address = buildAddressObject();
-        messageRequest.setAddress(address);
-        messageRequest.setCountry("India");
-        messageRequest.setCountryCode("IN");
-        messageRequest.setCustomerNumber("CUST123456");
-        messageRequest.setCustomerStatus(CustomerStatusEnum.OPEN);
-        messageRequest.setEmail("David@gmail.com");
-        messageRequest.setFirstName("pavan pavan");
-        messageRequest.setLastName("kumar kumar");
-        messageRequest.setBirthDate("12-02-2020");
-        messageRequest.setMobileNumber("9912101210");
-        return messageRequest;
-    }
-
-    private Address buildAddressObject() {
-        Address address = new Address();
-        address.setAddressLine1("lakkavaram");
-        address.setAddressLine2("west");
-        address.setPostalCode("534451");
-        address.setStreet("jungle");
-        return address;
-    }
-
-    private MessageResponse buildMessageResponse() {
-        MessageResponse response = new MessageResponse();
-        response.setData("Published Message sucessfully");
-        response.setStatus("success");
-        return response;
-    }
+	private MessageResponse buildMessageResponse() {
+		MessageResponse response = new MessageResponse();
+		response.setData("Published Message sucessfully");
+		response.setStatus("success");
+		return response;
+	}
 }
