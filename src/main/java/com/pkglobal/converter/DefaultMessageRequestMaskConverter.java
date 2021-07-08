c7034a63865495b99e73c0e23a705412b675e7ec
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-08 18:44:24 +0530

    commit

--- src/main/java/com/pkglobal/converter/DefaultMessageRequestMaskConverter.java
+++ src/main/java/com/pkglobal/converter/DefaultMessageRequestMaskConverter.java
@@ -4,15 +4,12 @@ import org.springframework.stereotype.Component;
 
 import com.pkglobal.constant.PublisherRegexConstants;
 import com.pkglobal.model.MessageRequest;
-import com.pkglobal.converter.MaskConverter;
 
 @Component
 public class DefaultMessageRequestMaskConverter implements MaskConverter<MessageRequest> {
 
 	@Override
 	public MessageRequest convert(MessageRequest messageRequest) {
-
-		
 
 		messageRequest.setCustomerNumber(messageRequest.getCustomerNumber().replaceAll(
 				(PublisherRegexConstants.CUSTOMER_NUMBER_REGEX_EXPRESSION.getRegexExpression()),
