15ae1cbe905843a6c304405a0022d53c15f47020
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-09 13:36:46 +0530

    commit

--- src/main/java/com/pkglobal/security/AuthorizationServerConfiguration.java
+++ src/main/java/com/pkglobal/security/AuthorizationServerConfiguration.java
@@ -2,16 +2,13 @@ package com.pkglobal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -20,38 +17,25 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 	public static final String CLIENT = "client";
 	public static final String CLIENT_PASSWORD = "password";
 	public static final String PASSWORD_GRANT_TYPE = "password";
-	public static final String AUTH_CODE_GRANT_TYPE = "authorization_code";
 	public static final String REFRESH_TOKEN_GRANT_TYPE = "refresh_token";
-	public static final String IMPLICIT_GRANT_TYPE = "implicit";
-	public static final String READ_SCOPE = "read";
-	public static final String WRITE_SCOPE = "write";
 
 	@Autowired
 	@Qualifier("authenticationManagerBean")
 	private AuthenticationManager authenticationManager;
 
 	@Autowired
-	private TokenStore tokenStore;
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
-		clients.inMemory().withClient(CLIENT)
-				.authorizedGrantTypes(PASSWORD_GRANT_TYPE, AUTH_CODE_GRANT_TYPE, REFRESH_TOKEN_GRANT_TYPE,
-						IMPLICIT_GRANT_TYPE)
-				.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT", "USER").scopes(READ_SCOPE, WRITE_SCOPE)
-				.autoApprove(true)
-				.secret(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(CLIENT_PASSWORD))
-				.accessTokenValiditySeconds(15 * 60);
+		clients.inMemory().withClient(CLIENT).authorizedGrantTypes(PASSWORD_GRANT_TYPE, REFRESH_TOKEN_GRANT_TYPE)
+				.secret(passwordEncoder.encode(CLIENT_PASSWORD)).accessTokenValiditySeconds(15 * 60);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore);
+		endpoints.authenticationManager(authenticationManager);
 	}
 
-	@Bean
-	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
-	}
 }
--- src/main/java/com/pkglobal/security/WebSecurityConfiguration.java
+++ src/main/java/com/pkglobal/security/WebSecurityConfiguration.java
@@ -8,7 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
@@ -28,11 +29,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public UserDetailsService userDetailsService() {
 
-		UserDetails user = User.builder().username(USER_NAME)
-				.password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(USER_PASSWORD))
+		UserDetails user = User.builder().username(USER_NAME).password(passwordEncoder().encode(USER_PASSWORD))
 				.roles("USER").build();
 
 		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();
 	}
 
 }
