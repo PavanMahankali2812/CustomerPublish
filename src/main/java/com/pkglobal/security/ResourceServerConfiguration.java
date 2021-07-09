578760d9f17415aad4cd0579923bb2d834eee9a0
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-09 14:01:38 +0530

    commit

--- src/main/java/com/pkglobal/security/ResourceServerConfiguration.java
+++ src/main/java/com/pkglobal/security/ResourceServerConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+import com.pkglobal.exceptions.GlobalExceptionHandler;
+
 @EnableResourceServer
 @Configuration
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
@@ -12,8 +14,6 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/oauth/token", "/oauth/authorize**").permitAll().anyRequest()
-				.authenticated();
-
+				.authenticated().and().exceptionHandling().authenticationEntryPoint(new GlobalExceptionHandler());
 	}
-
 }
