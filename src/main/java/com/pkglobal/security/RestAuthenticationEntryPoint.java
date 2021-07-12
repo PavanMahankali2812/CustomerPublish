49029e88d0916becaaab0dfb0202b881cf77cb97
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-12 12:20:59 +0530

    commit

--- src/main/java/com/pkglobal/security/RestAuthenticationEntryPoint.java
+++ src/main/java/com/pkglobal/security/RestAuthenticationEntryPoint.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
@@ -26,16 +24,14 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-		if (authException instanceof InsufficientAuthenticationException) {
-			if (authException.getCause() instanceof InvalidTokenException) {
-				ErrorResponse errorResponse = new ErrorResponse();
-				errorResponse.setStatus(PublisherConstants.ERROR.getValue());
-				errorResponse.setMessage(authException.getLocalizedMessage());
-				errorResponse.setErrorType("Unauthorized");
-				logger.error("ErrorResponse : {}", errorResponse);
-				final ObjectMapper mapper = new ObjectMapper();
-				mapper.writeValue(response.getOutputStream(), errorResponse);
-			}
-		}
+
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setStatus(PublisherConstants.ERROR.getValue());
+		errorResponse.setMessage(authException.getLocalizedMessage());
+		errorResponse.setErrorType("Unauthorized");
+		logger.error("ErrorResponse : {}", errorResponse);
+		final ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(response.getOutputStream(), errorResponse);
+
 	}
 }
