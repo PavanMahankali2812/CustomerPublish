b988a8cf558093e2f298273403ad1e2e8d7eeae0
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-14 17:16:52 +0530

    commit

--- src/main/java/com/pkglobal/security/ResourceServerConfiguration.java
+++ src/main/java/com/pkglobal/security/ResourceServerConfiguration.java
@@ -12,6 +12,8 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/oauth/token", "/oauth/authorize**").permitAll().anyRequest()
-				.authenticated().and().exceptionHandling().authenticationEntryPoint(new RestAuthenticationEntryPoint());
+				.authenticated();
+		// .and().exceptionHandling().authenticationEntryPoint(new
+		// RestAuthenticationEntryPoint()
 	}
 }
