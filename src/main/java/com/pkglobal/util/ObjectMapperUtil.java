1e138db60fdc6a49d25ea197fd57daa54d795842
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-14 11:49:00 +0530

    commit

--- src/main/java/com/pkglobal/util/ObjectMapperUtil.java
+++ src/main/java/com/pkglobal/util/ObjectMapperUtil.java
@@ -2,24 +2,27 @@ package com.pkglobal.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.pkglobal.exceptions.ApplicationRuntimeException;
 
 public class ObjectMapperUtil {
 
-    static final Logger LOGGER = LoggerFactory.getLogger(ObjectMapperUtil.class);
+	static final Logger LOGGER = LoggerFactory.getLogger(ObjectMapperUtil.class);
 
-    private ObjectMapperUtil() {
-        throw new AssertionError("No Object Creation");
-    }
+	private ObjectMapperUtil() {
+		throw new AssertionError("No Object Creation");
+	}
 
-    public static String returnJsonFromObject(Object object) throws Exception {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.writeValueAsString(object);
-        } catch (JsonProcessingException ex) {
-            throw new Exception();
-        }
-    }
+	public static String returnJsonFromObject(Object object) {
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			return objectMapper.writeValueAsString(object);
+		} catch (JsonProcessingException ex) {
+			throw new ApplicationRuntimeException(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()),
+					ex.getMessage());
+		}
+	}
 }
