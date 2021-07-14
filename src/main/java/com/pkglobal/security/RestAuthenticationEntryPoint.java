cc1e135b49523940d21e9b984e58bb943c29cfd7
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-14 17:11:02 +0530

    commit

--- src/main/java/com/pkglobal/security/RestAuthenticationEntryPoint.java
+++ src/main/java/com/pkglobal/security/RestAuthenticationEntryPoint.java
@@ -6,24 +6,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.pkglobal.constant.PublisherConstants;
+import com.pkglobal.model.ErrorResponse;
 
 @Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-	@Autowired
-	@Qualifier("handlerExceptionResolver")
-	private HandlerExceptionResolver resolver;
+	private static final Logger logger = LoggerFactory.getLogger(RestAuthenticationEntryPoint.class);
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-		resolver.resolveException(request, response, null, authException);
-	}
 
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setStatus(PublisherConstants.ERROR.getValue());
+		errorResponse.setMessage(authException.getLocalizedMessage());
+		errorResponse.setErrorType("Unauthorized");
+		logger.error("ErrorResponse : {}", errorResponse);
+		final ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(response.getOutputStream(), errorResponse);
+
+	}
 }
--- src/main/java/com/pkglobal/security/WebSecurityConfiguration.java
+++ src/main/java/com/pkglobal/security/WebSecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.pkglobal.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -18,6 +21,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	public static final String USER_NAME = "user";
 	public static final String USER_PASSWORD = "secret";
+
+	@Autowired
+	private AuthFilter filterChainExceptionHandler;
 
 	@Bean
 	@Override
@@ -39,6 +45,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 
 		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(filterChainExceptionHandler, CorsFilter.class);
+
 	}
 
 }
