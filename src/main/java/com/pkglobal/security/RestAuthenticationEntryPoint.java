805605e2b19be908c5fdd2c919229aeaf9653d4d
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-15 11:50:08 +0530

    commit

--- src/main/java/com/pkglobal/security/RestAuthenticationEntryPoint.java
+++ src/main/java/com/pkglobal/security/RestAuthenticationEntryPoint.java
@@ -8,28 +8,45 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import com.pkglobal.exceptions.GlobalExceptionHandler;
 
 @Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	private static final Logger logger = LoggerFactory.getLogger(RestAuthenticationEntryPoint.class);
-	@Autowired
-	@Qualifier("handlerExceptionResolver")
-	private HandlerExceptionResolver resolver;
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-		logger.info("---------------logger");
-		resolver.resolveException(request, response, GlobalExceptionHandler.class, authException);
+
+		/*
+		 * ErrorResponse errorResponse = new ErrorResponse();
+		 * errorResponse.setStatus(PublisherConstants.ERROR.getValue());
+		 * errorResponse.setMessage(authException.getLocalizedMessage());
+		 * errorResponse.setErrorType(authException.getClass().getSimpleName());
+		 * logger.error("ErrorResponse : {}", errorResponse); final ObjectMapper mapper
+		 * = new ObjectMapper(); mapper.writeValue(response.getOutputStream(),
+		 * errorResponse);
+		 */
+
+		if (authException instanceof InsufficientAuthenticationException) {
+
+			if (authException.getCause() instanceof InvalidTokenException) {
+				response.getOutputStream().println("{ " + "\"message\": \"Token has expired\","
+						+ "\"type\": \"Unauthorized\"," + "\"status\": 401" + "}");
+			}
+		}
+		if (authException instanceof AuthenticationCredentialsNotFoundException) {
+
+			response.getOutputStream().println("{ " + "\"message\": \"Missing Authorization Header\","
+					+ "\"type\": \"Unauthorized\"," + "\"status\": 401" + "}");
+		}
 
 	}
+
 }
