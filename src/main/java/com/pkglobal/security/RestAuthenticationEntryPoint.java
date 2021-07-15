d65a7e66b51881f4e7d8c11f5b6727068e22cf2b
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-15 11:14:01 +0530

    commit

--- src/main/java/com/pkglobal/security/RestAuthenticationEntryPoint.java
+++ src/main/java/com/pkglobal/security/RestAuthenticationEntryPoint.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,7 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	private static final Logger logger = LoggerFactory.getLogger(RestAuthenticationEntryPoint.class);
 	@Autowired
-	// @Qualifier("handlerExceptionResolver")
+	@Qualifier("handlerExceptionResolver")
 	private HandlerExceptionResolver resolver;
 
 	@Override
